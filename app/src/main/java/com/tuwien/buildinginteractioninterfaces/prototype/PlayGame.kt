93243390a2d5721413b7cc49e50d8cfec37d6240
444Duarte <duartemr.pinto@gmail.com>
2017-12-18 21:41:08 +0100

    Initial commit

---
+++ app/src/main/java/com/tuwien/buildinginteractioninterfaces/prototype/PlayGame.kt
@@ -0,0 +1,12 @@
+package com.tuwien.buildinginteractioninterfaces.prototype
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayGame : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play_game)
+    }
+}
