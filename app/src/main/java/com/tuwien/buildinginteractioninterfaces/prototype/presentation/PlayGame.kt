50a96c872316f7b23c6ec14fb1ab43bdc0ab9f2f
444Duarte <duartemr.pinto@gmail.com>
2017-12-19 11:15:20 +0100

    Started game

--- app/src/main/java/com/tuwien/buildinginteractioninterfaces/prototype/presentation/PlayGame.kt
+++ app/src/main/java/com/tuwien/buildinginteractioninterfaces/prototype/presentation/PlayGame.kt
@@ -2,12 +2,19 @@ package com.tuwien.buildinginteractioninterfaces.prototype.presentation
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.tuwien.buildinginteractioninterfaces.prototype.R
 
 class PlayGame : AppCompatActivity() {
 
+    lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play_game)
+
+        button = findViewById<Button>(R.id.start_button)
+
+
     }
 }
