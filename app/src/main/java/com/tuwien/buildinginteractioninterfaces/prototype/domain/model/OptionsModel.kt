abb9165ab122b34b5379a1a9f20b7f7c2a38db5f
444Duarte <duartemr.pinto@gmail.com>
2018-01-27 18:45:25 +0100

    Menu finished

--- app/src/main/java/com/tuwien/buildinginteractioninterfaces/prototype/domain/model/OptionsModel.kt
+++ app/src/main/java/com/tuwien/buildinginteractioninterfaces/prototype/domain/model/OptionsModel.kt
@@ -1,8 +1,9 @@
 package com.tuwien.buildinginteractioninterfaces.prototype.domain.model
 
+import java.io.Serializable
 
 
-class OptionsModel(var typeGame: TypeGame, var autoCorrect: Boolean) {
+class OptionsModel(var typeGame: TypeGame, var autoCorrect: Boolean) : Serializable{
     enum class TypeGame{
         TIME, NUM_WORDS, NUM_ERRORS, NUM_CORRECT_WORDS, TEXT, NO_END
     }
--- app/src/main/java/com/tuwien/buildinginteractioninterfaces/prototype/presentation/MainMenu.java
+++ app/src/main/java/com/tuwien/buildinginteractioninterfaces/prototype/presentation/MainMenu.java
@@ -1,14 +1,18 @@
 package com.tuwien.buildinginteractioninterfaces.prototype.presentation;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.tuwien.buildinginteractioninterfaces.prototype.R;
+import com.tuwien.buildinginteractioninterfaces.prototype.domain.model.OptionsModel;
 
 /**
  * Created by duarte on 27-01-2018.
@@ -23,10 +27,54 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_menu);
+
+        ((RadioGroup) findViewById(R.id.game_mode_group)).setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                onRadioButtonClicked(checkedId);
+            }
+        });
+
         Button button = (Button) findViewById(R.id.button_create_game);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                boolean autoCorrect = ((CheckBox) findViewById(R.id.main_menu_autocorrect)).isChecked();
+
+                int gameModeId = ((RadioGroup) findViewById(R.id.game_mode_group)).getCheckedRadioButtonId();
+                OptionsModel.TypeGame typeGame;
+
+                switch (gameModeId){
+                    case R.id.main_menu_time:
+                        typeGame = OptionsModel.TypeGame.TIME;
+                        break;
+                    case R.id.main_menu_num_words:
+                        typeGame = OptionsModel.TypeGame.NUM_WORDS;
+                        break;
+                    case R.id.main_menu_errors:
+                        typeGame = OptionsModel.TypeGame.NUM_ERRORS;
+                        break;
+                    case R.id.main_menu_correct_words:
+                        typeGame = OptionsModel.TypeGame.NUM_CORRECT_WORDS;
+                        break;
+                    case R.id.main_menu_text:
+                        typeGame = OptionsModel.TypeGame.TEXT;
+                        break;
+                    case R.id.main_menu_no_end:
+                        typeGame = OptionsModel.TypeGame.NO_END;
+                        break;
+                    default:
+                        typeGame = OptionsModel.TypeGame.TIME;
+                        break;
+                }
+
+
+                OptionsModel optionsModel = new OptionsModel(typeGame,autoCorrect);
+
+
+                startActivity(new Intent(MainMenu.this, PlayGame.class).putExtra("OPTIONS", optionsModel));
+
 
             }
         });
@@ -34,7 +82,9 @@ public class MainMenu extends AppCompatActivity {
     }
 
 
-    public void onRadioButtonClicked(View view) {
+    public void onRadioButtonClicked(int id) {
+        View view = findViewById(id);
+
         // Is the button now checked?
         boolean checked = ((RadioButton) view).isChecked();
         // Check which radio button was clicked
@@ -61,7 +111,7 @@ public class MainMenu extends AppCompatActivity {
                 break;
         case R.id.main_menu_no_end:
             if (checked)
-                ((TextView) findViewById(R.id.label_main_menu_num)).setVisibility(View.GONE);
+                findViewById(R.id.number_input_layout).setVisibility(View.GONE);
 
                 break;
 
@@ -74,6 +124,6 @@ public class MainMenu extends AppCompatActivity {
     private void displayNumberSelection(String text){
         TextView textView = (TextView) findViewById(R.id.label_main_menu_num);
         textView.setText(text);
-        textView.setVisibility(View.VISIBLE);
+        findViewById(R.id.number_input_layout).setVisibility(View.VISIBLE);
     }
 }
