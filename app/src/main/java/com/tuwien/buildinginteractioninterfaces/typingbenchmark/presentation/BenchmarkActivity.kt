c554ef24779f1acfb69e55692ddeebd5b5669de3
444Duarte <duartemr.pinto@gmail.com>
2018-03-30 19:29:13 +0100

    added copy icon to actionbar

--- app/src/main/java/com/tuwien/buildinginteractioninterfaces/typingbenchmark/presentation/BenchmarkActivity.kt
+++ app/src/main/java/com/tuwien/buildinginteractioninterfaces/typingbenchmark/presentation/BenchmarkActivity.kt
@@ -2,6 +2,8 @@ package com.tuwien.buildinginteractioninterfaces.typingbenchmark.presentation
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
 import com.tuwien.buildinginteractioninterfaces.typingbenchmark.R
 import com.tuwien.buildinginteractioninterfaces.typingbenchmark.domain.model.BenchmarkModel
@@ -13,7 +15,9 @@ class BenchmarkActivity: AppCompatActivity() {
         setContentView(R.layout.activity_benchmark)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         supportActionBar!!.setDisplayShowHomeEnabled(true)
-        val benchmark = intent.extras["BENCHMARK"] as BenchmarkModel?
+
+        val benchmark = intent.extras["BENCHMARK"] as BenchmarkModel
+        supportActionBar!!.title = benchmark.uid.toString()
 
         val benchmarkText = findViewById<TextView>(R.id.benchmark)
 
@@ -23,6 +27,25 @@ class BenchmarkActivity: AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean{
         onBackPressed()
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_copy-> {
+
+            true
+        }
+
+        else -> {
+            // If we got here, the user's action was not recognized.
+            // Invoke the superclass to handle it.
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.benchmark_menu, menu)
         return true
     }
 }
